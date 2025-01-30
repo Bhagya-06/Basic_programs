fc3a5ff5d98acb8998f26d69a6449b2aaa458241
Bhagya B <114055494+Bhagya-06@users.noreply.github.com>
2025-01-30 23:35:51 +0530

    Create simple_interest.c

---
+++ simple_interest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float P = 1, R = 1, T = 1;
+    // Calculate simple interest using formula
+    float SI = (P * T * R) / 100;
+    printf("Simple Interest = %f\n", SI);
+
+    return 0;
+}
